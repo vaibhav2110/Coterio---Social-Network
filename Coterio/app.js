0a61aa9f9f47ea62e5d813e511a9f97f6cd5224e
Vaibhav Prakash <vaibhpraky@gmail.com>
2018-03-23 21:48:19 +0530

    User Authentication done

---
+++ Coterio/app.js
@@ -0,0 +1,56 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var passport = require('passport');
+var authenticate = require('./authenticate');
+var app = express();
+var config = require('./config');
+// view engine setup
+
+
+const url = config.mongoUrl;
+const connect = mongoose.connect(url, {
+    useMongoClient: true,
+    /* other options */
+  });
+
+connect.then((db) => {
+    console.log("Connected correctly to server");
+}, (err) => { console.log(err); });
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ Coterio/routes/users.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var router = express.Router();
+const bodyParser = require('body-parser');
+var User = require('../models/user');
+
+var passport = require('passport');
+var authenticate = require('../authenticate');
+router.use(bodyParser.json());
+
+/* GET users listing. */
+router.post('/signup', (req, res, next) => {
+  User.register(new User({username: req.body.username}), 
+    req.body.password, (err, user) => {
+    if(err) {
+      res.statusCode = 500;
+      res.setHeader('Content-Type', 'application/json');
+      res.json({err: err});
+    }
+    else {
+     if (req.body.firstname)
+        user.firstname = req.body.firstname;
+      if (req.body.lastname)
+        user.lastname = req.body.lastname;
+      user.save((err, user) => {
+        if (err) {
+          res.statusCode = 500;
+          res.setHeader('Content-Type', 'application/json');
+          res.json({err: err});
+          return ;
+        }
+        passport.authenticate('local')(req, res, () => {
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'application/json');
+          res.json({success: true, status: 'Registration Successful!'});
+        });
+      });
+    }
+  });
+});
+
+router.post('/login', passport.authenticate('local'), (req, res) => {
+  var token = authenticate.getToken({_id: req.user._id});
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.json({success: true,token: token, status: 'You are successfully logged in!'});
+});
+
+module.exports = router;
