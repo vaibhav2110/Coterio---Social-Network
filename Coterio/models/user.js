d94493caefe08841310ad82cad4c05a2f00fb980
Vaibhav Prakash <vaibhpraky@gmail.com>
2018-03-25 18:14:37 +0530

    follow unfollow

--- Coterio/models/user.js
+++ Coterio/models/user.js
@@ -16,6 +16,8 @@ var User = new Schema({
         default: ""
     },
     favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Article' }],
+    following: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    followers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
 });
 
 User.methods.favorite = function(id) {
@@ -34,6 +36,45 @@ User.methods.unfavorite = function(id) {
 User.methods.isFavorite = function(id) {
   return this.favorites.some(function(favoriteId) {
     return favoriteId.toString() === id.toString();
+  });
+};
+
+User.methods.follow = function(id) {
+  if (this.following.indexOf(id) === -1) {
+    this.following.push(id);
+  }
+
+  return this.save();
+};
+
+User.methods.unfollow = function(id) {
+  this.following.remove(id);
+  return this.save();
+};
+
+User.methods.followed = function(id) {
+  if (this.followers.indexOf(id) === -1) {
+    this.follower.push(id);
+  }
+
+  return this.save();
+};
+
+User.methods.unfollowed = function(id) {
+  this.followers.remove(id);
+  return this.save();
+};
+
+User.methods.isFollowed = function(user, id) {
+  user = this;
+  return this.followers.some(function(followId) {
+    return followId.toString() === id.toString();
+  });
+};
+
+User.methods.isFollowing = function(id) {
+  return this.following.some(function(followId) {
+    return followId.toString() === id.toString();
   });
 };
 
--- Coterio/routes/users.js
+++ Coterio/routes/users.js
@@ -8,6 +8,7 @@ var authenticate = require('../authenticate');
 router.use(bodyParser.json());
 
 /* GET users listing. */
+
 router.post('/signup', (req, res, next) => {
   User.register(new User({username: req.body.username}), 
     req.body.password, (err, user) => {
@@ -44,5 +45,183 @@ router.post('/login', passport.authenticate('local'), (req, res) => {
   res.setHeader('Content-Type', 'application/json');
   res.json({success: true,token: token, status: 'You are successfully logged in!'});
 });
+router.get('/me', authenticate.verifyUser, (req,res,next)=>{
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.json(req.user);
+});
+router.get('/me/followers', authenticate.verifyUser, (req,res,next)=>{
+      User.findById(req.user._id)
+      .populate('followers')
+      .then((user)=>{
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'application/json');
+          res.json(user.followers);
+      }, (err)=> next(err))
+      .catch((err)=> next(err));  
+      
+});
+router.get('/me/following', authenticate.verifyUser, (req,res,next)=>{
+      User.findById(req.user._id)
+      .populate('following')
+      .then((user)=>{
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'application/json');
+          res.json(user.following);
+      }, (err)=> next(err))
+      .catch((err)=> next(err));  
+      
+});
+
+router.get('/me/favorites', authenticate.verifyUser, (req,res,next)=>{
+      User.findById(req.user._id)
+      .populate('favorites')
+      .then((user)=>{
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'application/json');
+          res.json(user.favorites);
+      }, (err)=> next(err))
+      .catch((err)=> next(err));  
+      
+});
+
+router.get('/:userId', authenticate.verifyUser, (req,res,next)=>{
+      User.findById(req.params.userId)
+      .then((user)=>{
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'application/json');
+          res.json(user);
+      }, (err)=> next(err))
+      .catch((err)=> next(err));     
+});
+
+router.post('/:userId/follow', authenticate.verifyUser, (req, res, next)=>{
+    if(req.user.isFollowing(req.params.userId)){
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({alreadyfollowing: 'true'});
+    }
+    else{
+        req.user.follow(req.params.userId)
+        .then((user)=>{
+            User.findById(req.params.userId)
+            .then((user)=>{
+                if(user.followers.indexOf(req.user._id)==-1){
+                    user.followers = user.followers.concat([req.user._id]);
+                }
+                user.save()
+                .then((user)=>{
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.json(user);
+                },(err)=> next(err))
+                .catch((err)=> next(err)); 
+            },(err)=> next(err))
+            .catch((err)=> next(err)); 
+        },(err)=> next(err))
+        .catch((err)=> next(err));
+    }
+    
+    /*User.findById(req.params.userId)
+    .then((user)=>{
+        console.log(user);
+        if(user.isFollowed(user, req.user._id)){
+           res.statusCode = 200;
+           res.setHeader('Content-Type', 'application/json');
+           res.json({alreadyfollowed: 'true'});
+           }
+        else{
+            user.followed(req.user._id)
+            .then((user)=>{
+                req.user.follow(user._id)
+                .then((user)=>{
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.json(user);
+                }, (err)=> next(err))
+                .catch((err)=> next(err));  
+            }, (err)=> next(err))
+            .catch((err)=> next(err));  
+        }
+    }, (err)=> next(err))
+      .catch((err)=> next(err));  */
+});
+router.post('/:userId/unfollow', authenticate.verifyUser, (req, res, next)=>{
+    if(!req.user.isFollowing(req.params.userId)){
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({alreadynotfollowing: 'true'});
+    }
+    else{
+        req.user.unfollow(req.params.userId)
+        .then((user)=>{
+            User.findById(req.params.userId)
+            .then((user)=>{
+                console.log(user.followers);
+                if(user.followers.indexOf(req.user._id)>-1){
+                    user.followers.splice(user.followers.indexOf(req.user._id),1);
+                }
+                user.save()
+                .then((user)=>{
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.json(user);
+                },(err)=> next(err))
+                .catch((err)=> next(err)); 
+            },(err)=> next(err))
+            .catch((err)=> next(err)); 
+        },(err)=> next(err))
+        .catch((err)=> next(err));
+    }
+    
+    /*User.findById(req.params.userId)
+    .then((user)=>{
+        console.log(user);
+        if(user.isFollowed(user, req.user._id)){
+           res.statusCode = 200;
+           res.setHeader('Content-Type', 'application/json');
+           res.json({alreadyfollowed: 'true'});
+           }
+        else{
+            user.followed(req.user._id)
+            .then((user)=>{
+                req.user.follow(user._id)
+                .then((user)=>{
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.json(user);
+                }, (err)=> next(err))
+                .catch((err)=> next(err));  
+            }, (err)=> next(err))
+            .catch((err)=> next(err));  
+        }
+    }, (err)=> next(err))
+      .catch((err)=> next(err));  */
+});
+/*router.post('/:userId/unfollow', authenticate.verifyUser, (req, res, next)=>{
+    User.findById(req.params.userId)
+    .then((user)=>{
+        if(!user.isFollowed(req.user._id)){
+           res.statusCode = 200;
+           res.setHeader('Content-Type', 'application/json');
+           res.json({alreadyunfollowed: 'true'});
+           }
+        else{
+            user.unfollowed(req.user._id)
+            .then((user)=>{
+                req.user.unfollow(user._id)
+                .then((user)=>{
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.json(user);
+                }, (err)=> next(err))
+                .catch((err)=> next(err));  
+            }, (err)=> next(err))
+            .catch((err)=> next(err));  
+        }
+    }, (err)=> next(err))
+      .catch((err)=> next(err));  
+});*/
+
 
 module.exports = router;
